f41a89cb7550d63e76dd0f16116a311094cfc49d
brskofdp <k.aissbga@gmail.com>
2026-08-09 04:11:39 +0200

    Fix formatting of RiskColor assignment in Library.lua

--- Library.lua
+++ Library.lua
@@ -34,7 +34,7 @@ local Library = {
     BackgroundColor = Color3.fromRGB(50, 50, 50);
     AccentColor = Color3.fromRGB(50, 50, 50);
     OutlineColor = Color3.fromRGB(50, 50, 50);
-    RiskColor = Color3.fromRGB(255, 50, 50),
+    RiskColor = Color3.fromRGB(255, 50, 50);
 
     Black = Color3.new(0, 0, 0);
     Font = Enum.Font.Code,
